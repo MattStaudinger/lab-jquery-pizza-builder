a149305ccb4f0c71e318540a47ad670251b9ac0d
Matthias Staudinger <staudinger.matthias@googlemail.com>
2018-10-16 16:11:49 +0200

    Add Iteration 3

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -13,37 +13,48 @@ var peppers = $(".green-pepper");
 var sauce = $('.sauce-white');
 var gluten = $('.crust-gluten-free');
 
-sauce.removeClass('.sauce-white');
-gluten.removeClass('.crust-gluten-free');
+sauce.removeClass('sauce-white');
+gluten.removeClass('crust-gluten-free');
+sauce.hasClass("sauce-white") ? sauceButton.addClass("active") : sauceButton.removeClass("active");
+gluten.hasClass("crust-gluten-free") ? glutenButton.addClass("active") : glutenButton.removeClass("active");
+
+
 
 // function checkTopping(element){
 //     if (element.css("display", "block");
 // }
 
-function checkActive(element) {
-    if (element.hasClass("active")) element.removeClass("active") ;
+
+function checkActive(ingredient, button) {
+    ingredient.hasClass("sauce-white") ? button.removeClass("active") : button.addClass("active");
 };
-checkActive(pepperoniButton);
+
 
 pepperoniButton.click(function() {
     pepperoni.toggle();
+    pepperoni.css("display") == "none" ? pepperoniButton.removeClass("active") : pepperoniButton.addClass("active");
 })
 
 mushroomsButton.click(function() {
     mushroom.toggle();
+    mushroom.css("display") == "none" ? mushroomsButton.removeClass("active") : mushroomsButton.addClass("active");
+
 })
 
 greenPeppersButton.click(function() {
     peppers.toggle();
+    peppers.css("display") == "none" ? greenPeppersButton.removeClass("active") : greenPeppersButton.addClass("active");
 })
 
 sauceButton.click(function() {
-    sauce.toggleClass();
+    sauce.toggleClass("sauce-white");
+    sauce.hasClass("sauce-white") ? sauceButton.addClass("active") : sauceButton.removeClass("active");
 })
 
 
 glutenButton.click(function(){
-    gluten.toggleClass()
+    gluten.toggleClass('crust-gluten-free')
+    gluten.hasClass("crust-gluten-free") ? glutenButton.addClass("active") : glutenButton.removeClass("active");
 })
 
 
